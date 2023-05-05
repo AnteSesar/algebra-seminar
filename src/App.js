9deadee89097153514bc5ec9ec5ae496b2292fcf
Ante <ante.sesar11@gmail.com>
2023-05-05 18:09:23 +0200

    no message error fix

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ class App extends React.Component {
   }  
 
   onSendMessage = (message) => {
+    if (message.length===0) return;
     this.drone.publish({
       room: "observable-room",
       message
