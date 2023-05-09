521ab5a2f7f3d65141f45df3ef91de1b6fc9cba2
Ante <ante.sesar11@gmail.com>
2023-05-09 16:56:28 +0200

    ready to do "router_and_login"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import './App.css';
-import './components/Messages';
 import Messages from './components/Messages';
-import React, { Component } from 'react';
-import Input from './components/input';
+import React from 'react';
+import Input from "./components/Input";
+import Sidebar from './components/Sidebar';
 
 function randomName() {
-  const adjectives = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
-  const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
-  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-  const noun = nouns[Math.floor(Math.random() * nouns.length)];
-  return adjective + noun;
+ const adjectives = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
+ const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
+ const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+ const noun = nouns[Math.floor(Math.random() * nouns.length)];
+ return adjective + noun;
 }
 
 function randomColor() {
-  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+ return '#' + Math.floor(Math.random() * 0xFFFFFA).toString(16);
 }
-
 class App extends React.Component {
 
   constructor() {
     super();
-    this.drone = new window.Scaledrone("YnAavhGS5tvZNcEr", {
+    this.drone = new window.Scaledrone("PnFavtIMvMsf69yV", {
       data: this.state.member
     });
     this.drone.on('open', error => {
@@ -31,15 +30,14 @@ class App extends React.Component {
       member.id = this.drone.clientId;
       this.setState({member});
     });
-
+ // ---
     const room = this.drone.subscribe("observable-room");
-
+ // ---
     room.on('data', (data, member) => {
       const messages = this.state.messages;
       messages.push({member, text: data});
       this.setState({messages});
     });
-    
   }
 
   state = {
@@ -48,9 +46,9 @@ class App extends React.Component {
       username: randomName(),
       color: randomColor()
     }
-  }  
-
+  }
   onSendMessage = (message) => {
+    // quick fix for sending empty message
     if (message.length===0) return;
     this.drone.publish({
       room: "observable-room",
@@ -58,20 +56,31 @@ class App extends React.Component {
     });
   }
 
-  render() {
+  toggleSidebar = () =>{
+    this.sidebar.ToggleSidebar();
+  }
+
+  render(){
     return (
       <div className="App">
-        <div className="App-header">
-        <h1>My Pain and Suffering App</h1>
+      <div className="App-header">
+        <button className='sidebar-btn' onClick={this.toggleSidebar}>Sidebar</button>
+        <h1>Algebra Seminarski Rad</h1>
+        {/* Prazan div služi za flex pozicioniranje elemenata */}
+        <div></div>
       </div>
-        <Messages
-          messages={this.state.messages}
-          currentMember={this.state.member}
-        />
-        <Input onSendMessage={this.onSendMessage}/>
-      </div>
+      <Sidebar ref={(reference)=> this.sidebar = reference}/>
+      <Messages
+        messages={this.state.messages}
+        currentMember={this.state.member}
+      />
+      <Input onSendMessage={this.onSendMessage}/>
+    </div>
     );
   }
+  
+
+
 }
 
 export default App;
