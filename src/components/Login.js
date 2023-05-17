7e5b976a7f1df7c5b3867c366c174148f0dfe982
Ante <ante.sesar11@gmail.com>
2023-05-17 17:10:46 +0200

    commitam ovo sto sam mjenjo

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,21 +6,29 @@ const Login = (props) =>{
     const [usernameText, setUsernameText] = useState("");
     const navigate = useNavigate();
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e)=>{
         e.preventDefault();
         props.onLogin(usernameText);
-        navigate("/");
+        navigate("/algebra-seminar/");        
     }
-
-    return <div className="container">
+    
+    return (
+      
+    <div className="container">
         <form className="login-form" onSubmit={handleSubmit}>
             <label htmlFor="title">
                 <h1>Login</h1>
             </label>
-            <input id="title" type="text" placeholder="Enter your username" required value={usernameText} onChange={(e)=> setUsernameText(e.target.value)}/>
+            <input 
+            id="title"
+            type="text" 
+            placeholder="Enter your username" 
+            required 
+            value={usernameText} 
+            onChange={(e)=> setUsernameText(e.target.value)}/>
             <button className="login-btn">Login</button>
         </form>
-    </div>
+    </div>)
 }
 
 export default Login;
